f5b1cf3ae856184b5daafd2105c6561e9ee026bf
AlexGS00 <acortojan@gmail.com>
2025-05-25 10:52:00 +0300

    add separate institution model

--- SmartCity/citysay/models.py
+++ SmartCity/citysay/models.py
@@ -1,27 +1,27 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+class Institution(models.Model):
+    code = models.CharField(max_length=10, unique=True)
+    name = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return self.name
+
 # Create your models here.
 class User(AbstractUser):
-    
-    INSTITUTIN_NAMES = [
-        ("PEDA" , 'Colegiul National Pedagogic "Regina Maria" Deva'),
-        ("POLICE" , "Politia Deva"),
-        ("LUCI" , "Primaria Municipiului Deva"),
-        ("ISJ" , "Inspectoratul Scolar Judetean Hunedoara"),
-        ("CJH", "Consiliul Judetean Hunedoara"),
-        ("NONE", "None"),
-    ]
     
     cnp = models.CharField(max_length=13)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
-    institution = models.CharField(max_length=200, choices=INSTITUTIN_NAMES)
+    institution = models.ForeignKey(Institution, null=True, blank=True, on_delete=models.SET_NULL)
+
     
 class Poll(models.Model):
     creator = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     description = models.CharField(max_length=2000)
+    institution = models.ForeignKey(Institution, null=True, blank=True, on_delete=models.SET_NULL)
 
 class Option(models.Model):
     poll = models.ForeignKey(Poll, on_delete=models.CASCADE)
@@ -32,4 +32,3 @@ class Sesization(models.Model):
     creator = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     description = models.CharField(max_length=2000)
-
