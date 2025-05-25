4f4c1219da55e5821a12b046d97d0b642cb3d340
AlexGS00 <acortojan@gmail.com>
2025-05-25 14:22:19 +0300

    create script for create_poll

---
+++ SmartCity/citysay/static/script/create_poll.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", function() {
+    num_options = document.getElementById("num-options");
+    addBtn = document.getElementById("add-btn");
+    removeBtn = document.getElementById("rm-btn");
+    optionsSection = document.getElementById("options");
+
+    addBtn.addEventListener("click", function() {
+        newOptionNum = parseInt(num_options.value) + 1;
+        num_options.value = newOptionNum;
+        var newOption = document.createElement("div");
+        newOption.className = "option";
+        newOption.innerHTML = `
+        <label for="option-${newOptionNum}" class="form-label">Optiunea ${newOptionNum}:</label>
+        <input name="option-${newOptionNum}" id="option-${newOptionNum}" type="text" autocomplete="off" placeholder="Optiunea ${newOptionNum}"></input>
+        `
+        optionsSection.appendChild(newOption)
+
+    });
+    removeBtn.addEventListener("click", function() {
+        if (parseInt(num_options.value) > 2) {
+            var lastOption = optionsSection.lastElementChild;
+            optionsSection.removeChild(lastOption);
+            num_options.value = parseInt(num_options.value) - 1;
+        } else {
+            alert("Trebuie sa existe cel putin doua optiuni!");
+        }
+    });
+});
