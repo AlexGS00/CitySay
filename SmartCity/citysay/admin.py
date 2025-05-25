9ca5e7ef6886e01da1359a8eca7478767dfe5393
AlexGS00 <acortojan@gmail.com>
2025-05-25 10:51:44 +0300

    add admin interface

--- SmartCity/citysay/admin.py
+++ SmartCity/citysay/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import User, Poll, Option, Sesization, Institution
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Poll)
+admin.site.register(Option)
+admin.site.register(Sesization)
+admin.site.register(Institution)
