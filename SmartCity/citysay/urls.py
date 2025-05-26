204b0bb51d39e804b596550afc8a8e383bc05286
AlexGS00 <acortojan@gmail.com>
2025-05-26 17:34:21 +0300

    add vote and sesization path

--- SmartCity/citysay/urls.py
+++ SmartCity/citysay/urls.py
@@ -9,7 +9,8 @@ urlpatterns = [
     path("register", views.register, name="register"),
     path("polls", views.polls, name="polls"),
     path("poll/<int:poll_id>", views.poll, name="poll"),
+    path("vote/<int:poll_id>/<int:option_id>", views.vote, name="vote"),
     path("create_poll", views.create_poll, name="create_poll"),
-    path("sesizations", views.sesizations, name="sesizations"),
+    path("create_sesization", views.create_sesization, name="create_sesization"),
     
 ]
