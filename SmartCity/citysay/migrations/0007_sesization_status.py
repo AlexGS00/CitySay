4e197f84c91cd3367d6f2ad615438fc8a08c92ee
AlexGS00 <acortojan@gmail.com>
2025-09-04 16:34:50 +0300

    Solve migration error

--- SmartCity/citysay/migrations/0007_sesization_status.py
+++ SmartCity/citysay/migrations/0007_sesization_status.py
@@ -13,7 +13,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='sesization',
             name='status',
-            field=models.CharField(default='în asteptare...', max_length=10),
+            field=models.CharField(default='în asteptare...', max_length=30),
             preserve_default=False,
         ),
     ]
